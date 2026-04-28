6bc92e2131067c69b2c5c96538a20c3dac0ec016
Sebas <sebasterco10@gmail.com>
2026-04-28 16:24:14 -0300

    Bitacora + Reflej

--- js/script.js
+++ js/script.js
@@ -38,3 +38,42 @@ window.addEventListener("DOMContentLoaded", () => {
     // Crear burbujas cada 300ms
     setInterval(createBubble, 400);
 });
+
+const btnArriba = document.getElementById('btn-arriba');
+const btnAbajo = document.getElementById('btn-abajo');
+
+// subir 
+btnArriba.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
+
+// bajar 
+btnAbajo.addEventListener('click', () => {
+    window.scrollTo({
+        top: document.body.scrollHeight,
+        behavior: 'smooth'
+    });
+});
+
+// Ocultar botones al hacer scroll
+window.addEventListener('scroll', () => {
+    const scrollY = window.scrollY;
+    const alturaPantalla = window.innerHeight;
+    const alturaTotal = document.body.scrollHeight;
+    // boton subir
+    if (window.scrollY > 50) {
+        btnArriba.style.opacity = "1";
+    } else {
+        btnArriba.style.opacity = "0";
+    }
+
+    // boton bajar
+    if (scrollY + alturaPantalla >= alturaTotal - 10) {
+        btnAbajo.style.opacity = "0"; // estás abajo del todo
+    } else {
+        btnAbajo.style.opacity = "1";
+    }
+});
